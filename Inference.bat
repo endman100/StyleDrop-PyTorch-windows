21724f565437109d00f3920c8634acda3b684eb7
endman100 <endman246738@gmail.com>
2023-11-13 17:43:07 +0800

    push inference bat

--- Inference.bat
+++ Inference.bat
@@ -1,4 +1,4 @@
-set EVAL_CKPT="assets/ckpts/cc3m-285000.ckpt" 
-set ADAPTER="./style_adapter/0102.pth"
-set OUTPUT_DIR="./output"
-accelerate launch --num_processes 8 --mixed_precision fp16 train_t2i_custom_v2.py --config=configs/custom.py
+set EVAL_CKPT=assets/ckpts/cc3m-285000.ckpt
+set ADAPTER=./style_adapter/0102.pth
+set OUTPUT_DIR=./output
+accelerate launch --num_processes 1 --mixed_precision fp16 train_t2i_custom_v2.py --config=configs/custom.py
